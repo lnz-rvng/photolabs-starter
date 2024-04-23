1d416b7248fd03e6cf6a5daee054e1f32cb36c61
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 02:03:52 -0600

    move the data processing logic to its parent component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,35 +5,31 @@ import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   const { data } = props;
+  const { id, location, urls, user } = data;
+  const { city, country } = location;
+  const { full, regular } = urls;
+  const { username, name, profile } = user;
 
-  const mappedData = data.map((item) => {
-    const { id, location, urls, user } = item;
-    const { city, country } = location;
-    const { full, regular } = urls;
-    const { username, name, profile } = user;
-    return (
-      <div key={id} className="photo-list__item">
-        <PhotoFavButton />
-        <img className="photo-list__image" src={full} alt="display photo" />
-        <div className="photo-list__user-details">
-          <img
-            className="photo-list__user-profile"
-            src={profile}
-            alt="profile picture"
-          />
-          <div className="photo-list__user-info">
-            <span>{name}</span>
-            <br />
-            <span className="photo-list__user-location">
-              {city}, {country}
-            </span>
-          </div>
+  return (
+    <div key={id} className="photo-list__item">
+      <PhotoFavButton />
+      <img className="photo-list__image" src={full} alt="display photo" />
+      <div className="photo-list__user-details">
+        <img
+          className="photo-list__user-profile"
+          src={profile}
+          alt="profile picture"
+        />
+        <div className="photo-list__user-info">
+          <span>{name}</span>
+          <br />
+          <span className="photo-list__user-location">
+            {city}, {country}
+          </span>
         </div>
       </div>
-    );
-  });
-
-  return <div>{mappedData}</div>;
+    </div>
+  );
 };
 
 export default PhotoListItem;
