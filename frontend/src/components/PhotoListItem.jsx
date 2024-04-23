6ad9a1e1d3772a70a09ec3aed682bb942b7edcd2
Lance <lanceirvingmanzanal@icloud.com>
2024-04-22 23:30:11 -0600

    Add the PhotoFavIcon component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   const {
@@ -13,6 +14,7 @@ const PhotoListItem = (props) => {
   /* Insert React */
   return (
     <div className="photo-list__item">
+      <PhotoFavButton />
       <img
         className="photo-list__image"
         src={imageSource}
