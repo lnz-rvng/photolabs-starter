9130593a81c699276f61b34ee37a36076897f54b
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 02:30:15 -0600

    pass down the mock data as props to TopicList component

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -5,10 +5,11 @@ import TopicList from "./TopicList";
 import FavBadge from "./FavBadge";
 
 const TopNavigation = (props) => {
+  const { topics } = props;
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList />
+      <TopicList topics={topics}/>
       <FavBadge />
     </div>
   );
