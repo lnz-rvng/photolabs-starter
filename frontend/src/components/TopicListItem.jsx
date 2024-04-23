af204f0b996aba2512958ddc8d51fef1ca4a347d
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 00:45:49 -0600

    map the data that was passed down as props from topiclist component and render it

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
-
-const TopicListItem = () => {
-  return (
-    <div className="topic-list__item">
-      {/* Insert React */}
-    </div>
-  );
+const TopicListItem = (props) => {
+  const { data } = props;
+  const mappedData = data.map((item) => {
+    console.log(item)
+    const { id, slug, title } = item;
+    return (
+      <div key={id} className="topic-list__item">
+        <span>{title}</span>
+      </div>
+    );
+  });
+  return <>{mappedData}</>;
 };
 
 export default TopicListItem;
