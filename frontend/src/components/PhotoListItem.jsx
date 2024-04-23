2e40efb7d5a856ccfc527e1804b0e25b96a1ef7d
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 00:09:31 -0600

    render the mappedData using the passed down props from photolist component and display 3 diffrent photo

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,38 +4,36 @@ import "../styles/PhotoListItem.scss";
 import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
-  const {
-    imageSource,
-    profile,
-    username,
-    location: { city, country },
-    id,
-  } = props.photo;
-  /* Insert React */
-  return (
-    <div className="photo-list__item">
-      <PhotoFavButton />
-      <img
-        className="photo-list__image"
-        src={imageSource}
-        alt="display photo"
-      />
-      <div className="photo-list__user-details">
-        <img
-          className="photo-list__user-profile"
-          src={profile}
-          alt="profile picture"
-        />
-        <div className="photo-list__user-info">
-          <span>{username}</span>
-          <br />
-          <span className="photo-list__user-location">
-            {city}, {country}
-          </span>
+  const { data } = props;
+
+  const mappedData = data.map((item) => {
+    const { id, location, urls, user } = item;
+    const { city, country } = location;
+    const { full, regular } = urls;
+    const { username, name, profile } = user;
+    return (
+      <div key={id} className="photo-list__item">
+        <PhotoFavButton />
+        <img className="photo-list__image" src={full} alt="display photo" />
+        <div className="photo-list__user-details">
+          <img
+            className="photo-list__user-profile"
+            src={profile}
+            alt="profile picture"
+          />
+          <div className="photo-list__user-info">
+            <span>{name}</span>
+            <br />
+            <span className="photo-list__user-location">
+              {city}, {country}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  });
+
+  return <div>{mappedData}</div>;
 };
 
 export default PhotoListItem;
