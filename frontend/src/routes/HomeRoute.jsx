758d0988c50c8e7f6dabfe101f875361b3ba22e1
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 02:05:49 -0600

    create a component to view photos from the homepage

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-
-import '../styles/HomeRoute.scss';
-
-const HomeRoute = () => {
+import React from "react";
+import TopNavigation from "components/TopNavigationBar";
+import PhotoList from "components/PhotoList";
+import "../styles/HomeRoute.scss";
+function HomeRoute() {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation />
+      <PhotoList />
     </div>
   );
-};
+}
 
 export default HomeRoute;
