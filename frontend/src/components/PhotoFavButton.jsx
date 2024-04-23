d5b783d59eeb0671d3a482517086a74c5d3ac3e9
Lance <lanceirvingmanzanal@icloud.com>
2024-04-22 23:28:09 -0600

    Add a useState and pass down props onto FavIcon component

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,18 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton() {
+  const [isFav, setIsFav] = useState(false);
+
+  const clickHandler = () => {
+    setIsFav(!isFav);
+  };
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon onClick={clickHandler} selected={isFav} />
       </div>
     </div>
   );
