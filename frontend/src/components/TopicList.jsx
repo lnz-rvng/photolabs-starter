1761aedc23a583508a3c4b1920eb72b32b52bffa
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 02:29:51 -0600

    omit the sample data and replace it with the mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -3,35 +3,18 @@ import React from "react";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const mappedSampleDataForTopicList = sampleDataForTopicList.map((topic) => {
-  return (
-    <div key={topic.id}>
-      <TopicListItem title={topic.title} />
-    </div>
-  );
-});
-const TopicList = () => {
+const TopicList = (props) => {
+  const { topics } = props;
+  const mappedTopics = topics.map((topic) => {
+    return (
+      <div key={topic.id}>
+        <TopicListItem title={topic.title} />
+      </div>
+    );
+  });
   return (
     <div className="top-nav-bar__topic-list">
-      {mappedSampleDataForTopicList}
+      {mappedTopics}
     </div>
   );
 };
