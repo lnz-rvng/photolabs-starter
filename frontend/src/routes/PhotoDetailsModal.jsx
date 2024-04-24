ad26949cf9810cf676eeaff0146b996c2fbe4123
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 21:38:54 -0600

    receive props from the App component and use it as the event handler ofr the button

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -4,9 +4,10 @@ import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
 
 const PhotoDetailsModal = (props) => {
+  const { modal, toggleModal } = props;
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+      <button className="photo-details-modal__close-button" onClick={toggleModal}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
