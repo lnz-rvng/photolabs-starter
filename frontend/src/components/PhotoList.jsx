9490cb7da0970d1021755a1a9caae5a821aa94e3
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 21:31:53 -0600

    pass down props to PhotoListItem component

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -4,11 +4,19 @@ import "../styles/PhotoList.scss";
 import PhotoListItem from "./PhotoListItem";
 
 const PhotoList = (props) => {
-  const { photos, favoritedPhotos, setFavoritedPhotos } = props;
+  const { photos, favoritedPhotos, setFavoritedPhotos, toggleModal } = props;
   const mappedPhotos = photos.map((photo) => {
-    return <PhotoListItem key={photo.id} data={photo} favoritedPhotos={favoritedPhotos} setFavoritedPhotos={setFavoritedPhotos}/>;
+    return (
+      <PhotoListItem
+        key={photo.id}
+        data={photo}
+        favoritedPhotos={favoritedPhotos}
+        setFavoritedPhotos={setFavoritedPhotos}
+        toggleModal={toggleModal}
+      />
+    );
   });
-  
+
   return <div className="photo-list">{mappedPhotos}</div>;
 };
 
