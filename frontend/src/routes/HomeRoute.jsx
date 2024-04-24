14a34791e8d0f60e09a1e565c0e347f0cce351ad
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 19:15:23 -0600

    Add a state for the favorited photos and pass it down as props to the TopNavigation and PhotoList components

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import TopNavigation from "components/TopNavigationBar";
 import PhotoList from "components/PhotoList";
 import "../styles/HomeRoute.scss";
 function HomeRoute(props) {
   const { photos, topics } = props;
+  const [favoritedPhotos, setFavoritedPhotos] = useState([]);
   return (
     <div className="home-route">
-      <TopNavigation topics={topics}/>
-      <PhotoList photos={photos}/>
+      <TopNavigation topics={topics} favoritedPhotos={favoritedPhotos}/>
+      <PhotoList photos={photos} favoritedPhotos={favoritedPhotos} setFavoritedPhotos={setFavoritedPhotos}/>
     </div>
   );
 }
