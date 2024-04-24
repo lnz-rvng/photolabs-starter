b57333305f7cc1fffe1bdd4a3af97b935029ccfd
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 19:20:37 -0600

    pass down props to FavIcon component

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,16 +3,13 @@ import React, { useCallback, useState } from "react";
 import FavIcon from "./FavIcon";
 import "../styles/PhotoFavButton.scss";
 
-function PhotoFavButton() {
-  const [isFav, setIsFav] = useState(false);
+function PhotoFavButton(props) {
+  const { onClick, isFavorited } = props;
 
-  const clickHandler = () => {
-    setIsFav(prevIsFav => !prevIsFav);
-  };
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        <FavIcon onClick={clickHandler} selected={isFav} />
+        <FavIcon onClick={onClick} selected={isFavorited} />
       </div>
     </div>
   );
