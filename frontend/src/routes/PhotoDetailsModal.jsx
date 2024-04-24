dfef7581e0cecb328796a4c3eb997f1b5ae10b64
Lance <lanceirvingmanzanal@icloud.com>
2024-04-23 22:26:12 -0600

    map over the photos data from props

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -2,14 +2,27 @@ import React from "react";
 
 import "../styles/PhotoDetailsModal.scss";
 import closeSymbol from "../assets/closeSymbol.svg";
+import PhotoList from "components/PhotoList";
 
 const PhotoDetailsModal = (props) => {
-  const { modal, toggleModal } = props;
+  const { modal, toggleModal, photos } = props;
+ 
+  const mappedPhotos = photos.map((photo) => {
+    const { id, location, urls, user } = photo;
+    const { city, country } = location;
+    const { full, regular } = urls;
+    const { username, name, profile } = user;
+    return <div key={id}>{console.log(photo)}</div>;
+  });
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={toggleModal}>
+      <button
+        className="photo-details-modal__close-button"
+        onClick={toggleModal}
+      >
         <img src={closeSymbol} alt="close symbol" />
       </button>
+      {mappedPhotos}
     </div>
   );
 };
