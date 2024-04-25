9029c4a1c5ccd251f5e1f16550ada5941385ed9d
Lance <lanceirvingmanzanal@icloud.com>
2024-04-24 20:32:38 -0600

    reuse the PhotoList component to display similar photo on the modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,8 +5,9 @@ import closeSymbol from "../assets/closeSymbol.svg";
 import PhotoList from "components/PhotoList";
 
 const PhotoDetailsModal = (props) => {
-  const { modal, toggleModal, selectedPhoto } = props;
-  const { id, location, similar_photo, urls, user } = selectedPhoto;
+  const { toggleModal, selectedPhoto, favoritedPhotos, setFavoritedPhotos } =
+    props;
+  const { id, location, similar_photos, urls, user } = selectedPhoto;
 
   return (
     <div className="photo-details-modal">
@@ -16,7 +17,47 @@ const PhotoDetailsModal = (props) => {
       >
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      {console.log(selectedPhoto)}
+
+      <div className="photo-details-modal__top-bar">
+
+        <img
+          src={urls.full}
+          alt="larger display photo"
+          className="photo-details-modal__image"
+        />
+
+        <div className="photo-details-modal__photographer-details">
+          <img
+            className="photo-details-modal__photographer-profile"
+            src={user.profile}
+            alt="profile picture"
+          />
+          
+          <div className="photo-details-modal__photographer-info">
+            <span>{user.name}</span>
+            <br />
+            <span className="photo-details-modal__photographer-location">
+              {location.city}, {location.country}
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        
+        <header className="photo-details-modal__header">
+          <span>Similar Photos</span>
+        </header>
+
+        <div className="photo-details-modal__images">
+          <PhotoList
+            photos={Object.values(similar_photos)}
+            favoritedPhotos={favoritedPhotos}
+            setFavoritedPhotos={setFavoritedPhotos}
+          />
+        </div>
+
+      </div>
     </div>
   );
 };
