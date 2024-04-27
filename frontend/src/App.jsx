b791879cd7c48b8445e64b8d23ab14c5c7206e55
Lance <lanceirvingmanzanal@icloud.com>
2024-04-26 21:32:46 -0600

    remove the mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 
 import "./App.scss";
 import HomeRoute from "routes/HomeRoute";
-import photos from "../src/mocks/photos";
-import topics from "../src/mocks/topics";
 import PhotoDetailsModal from "routes/PhotoDetailsModal";
 import useApplicationData from "hooks/useApplicationData";
 
@@ -22,8 +20,6 @@ const App = () => {
   return (
     <div className="App">
       <HomeRoute
-        photos={photos}
-        topics={topics}
         toggleModal={toggleModal}
         setSelectedPhoto={setSelectedPhoto}
         favoritedPhotos={favoritedPhotos}
