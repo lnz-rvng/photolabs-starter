5712ac9845e14f5e6c87543068c791d3be1cbc24
Lance <lanceirvingmanzanal@icloud.com>
2024-05-02 16:45:25 -0600

    component for the dark mode button

---
+++ frontend/src/components/DarkModeButton.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "../styles/DarkModeButton.scss";
+
+function DarkModeButton(props) {
+  const { toggleDarkMode, ...state } = props;
+  return (
+    <div>
+      <button onClick={toggleDarkMode}>
+        {state.isDarkMode && (
+          <span>
+            <i>☀️</i>
+          </span>
+        )}
+        {!state.isDarkMode && (
+          <span>
+            <i>🌑</i>
+          </span>
+        )}
+      </button>
+    </div>
+  );
+}
+
+export default DarkModeButton;
